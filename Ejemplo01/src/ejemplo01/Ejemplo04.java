a390e167a742e937a6b6639d40dd51101b438a56
jonathan <jonathanjavier2004@hotmail.com>
2025-10-29 12:07:12 -0500

    hola 5

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -18,5 +18,12 @@ public class Ejemplo04 {
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
+        // pintln imprime toda la linea, mientras que print imprime de forma continua
+        
+        System.out.println("-------------------------------");
+        System.out.println(nombreEstudiante + " ");
+        System.out.println(apellidoEstudiante + " ");
+        System.out.println(nacimiento + " ");
+        System.out.println(nombreEstudiante + " ");
     }
 }
