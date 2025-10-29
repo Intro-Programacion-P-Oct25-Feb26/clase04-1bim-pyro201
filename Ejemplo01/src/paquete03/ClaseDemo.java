146ba4c69b1a2bbd6bfe196807aee238b15f3f43
jonathan <jonathanjavier2004@hotmail.com>
2025-10-29 11:34:17 -0500

    hola 2

---
+++ Ejemplo01/src/paquete03/ClaseDemo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author A S U S
+ */
+public class ClaseDemo {
+    
+    public static void main(String[] args) {
+        System.out.println("que tal su dia?");
+        
+        
+    }
+}
