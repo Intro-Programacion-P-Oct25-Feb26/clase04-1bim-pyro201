de7e5c10a1501756903c4cbeda5946435244a3ae
jonathan <jonathanjavier2004@hotmail.com>
2025-10-29 11:24:07 -0500

    hola

---
+++ Ejemplo01/src/paquete02/Ejercicio01.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete02;
+
+/**
+ *
+ * @author A S U S
+ */
+public class Ejercicio01 {
+    
+    
+}
